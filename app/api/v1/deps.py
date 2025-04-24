341c428b8fc3e969cd5b9524b57fd7a45961f6b5
Roman <iamromanpleshkov@gmail.com>
2025-04-25 01:17:16 +0300

    added request form dep

--- app/api/v1/deps.py
+++ app/api/v1/deps.py
@@ -1,7 +1,9 @@
 from typing import Annotated
 from fastapi import Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from app.database import db_connector
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
 SessionDep = Annotated[AsyncSession, Depends(db_connector.get_session)]
+RequestForm = Annotated[OAuth2PasswordRequestForm, Depends()]
