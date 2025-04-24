72d5e7016699e3c7de018bf44089aeb7db571e29
Roman <iamromanpleshkov@gmail.com>
2025-04-25 01:17:29 +0300

    created login endpoint

--- app/api/v1/auth_router.py
+++ app/api/v1/auth_router.py
@@ -1,6 +1,14 @@
 from fastapi import APIRouter
+from app.api.v1.deps import SessionDep, RequestForm
+from app.api.v1.services.auth_service import AuthService
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
 
-
+@router.post("/login")
+async def login(session: SessionDep, form_data: RequestForm):
+    return await AuthService.login(
+        session=session,
+        email=form_data.username,
+        password=form_data.password,
+    )
