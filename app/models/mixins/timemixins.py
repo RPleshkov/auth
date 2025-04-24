9e19a8edb3fa77ad803979fcb346f4082ce9fbbb
Roman <iamromanpleshkov@gmail.com>
2025-04-24 21:41:38 +0300

    create time mixins

---
+++ app/models/mixins/timemixins.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class CreatedAtMixin:
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+
+
+class UpdatedAtMixin:
+    updated_at: Mapped[datetime | None] = mapped_column(
+        DateTime(timezone=True), server_onupdate=func.now()
+    )
