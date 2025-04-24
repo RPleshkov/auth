6a780331b45516c69a5444e6755b486089432856
Roman <iamromanpleshkov@gmail.com>
2025-04-24 23:48:16 +0300

    add create_user endpoint

--- app/api/v1/user_router.py
+++ app/api/v1/user_router.py
@@ -1,9 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+
+from app.api.v1.deps import SessionDep
 from app.api.v1.schemas.user import UserCreate, UserPublic
+from app.api.v1.services.user_service import UserService
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
-@router.post("/register")
-async def register(user_in: UserCreate) -> UserPublic:
-    pass
+@router.post(
+    "/register",
+    response_model=UserPublic,
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_user(session: SessionDep, user_in: UserCreate):
+    return await UserService.create_user(session=session, user_in=user_in)
