4d4a3f69e904191c5bb6a4a2b50a0245c83e1bbc
Roman <iamromanpleshkov@gmail.com>
2025-04-24 22:13:45 +0300

    fix metadata

--- app/models/base.py
+++ app/models/base.py
@@ -3,13 +3,16 @@ import uuid
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import MetaData
+from app.core.config import settings
 
 
 class Base(DeclarativeBase):
 
     __abstract__ = True
 
-    metadata = MetaData(naming_convention=)
+    metadata = MetaData(
+        naming_convention=settings.postgres.naming_convention,
+    )
 
     id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True), primary_key=True, default=uuid.uuid4()
