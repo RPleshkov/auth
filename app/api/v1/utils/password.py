0ae95d44bc0c58b156d414cb9d5c46dfd86eae1b
Roman <iamromanpleshkov@gmail.com>
2025-04-24 23:26:20 +0300

    created hash and check password funcs

---
+++ app/api/v1/utils/password.py
@@ -0,0 +1,19 @@
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(
+        password=password.encode(),
+        salt=salt,
+    )
+
+
+def check_password(
+    password: str,
+    hashed_password: bytes,
+) -> bool:
+    return bcrypt.checkpw(
+        password=password.encode(),
+        hashed_password=hashed_password,
+    )
